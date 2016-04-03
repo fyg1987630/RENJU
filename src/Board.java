97114366ed08e6e1274a99296afc91b410279ec9
Hill FENG <yungangf@student.unimelb.edu.au>
2016-04-03 21:07:06 +1000

    Board class

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,14 @@
 
 public class Board {
-
+	public int allPieces[][];
+	
+	public void print(){
+		
+	}
+	
+	public void initialize(){
+		for(int i=0;i<15;i++)
+			for(int j=0;j<15;j++)
+				allPieces[i][j]=0;
+	}
 }
